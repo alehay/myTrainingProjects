110d784865c5aa83d667e2da5903d0fc6e93f6ae
alehay <Alehaymkm@gmail.com>
2020-03-09 22:02:07 +0300

    решено задание 17

--- Py/exercises/matrix/matrix_ex_17_polykov_p10.py
+++ Py/exercises/matrix/matrix_ex_17_polykov_p10.py
@@ -14,3 +14,48 @@
 
 заметки по решению :
 """
+
+def printSeparator ( A, name ):
+    print()
+    for i in range (3):
+        print ( A * 40)
+    print()
+    print (A *15,name, A*15)
+    print ()
+
+def printMatrix ( A ):
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            print ("{:3d}".format(A[i][j]), end ="")
+        print() #для перевода на новую строку. 
+N=3
+M=4
+matrix_1 = []
+num = 1
+matrix_1 = [[0]*M for i in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        matrix_1 [i][j] = num
+        num +=1
+printMatrix (matrix_1)
+
+printSeparator ("*","matrix2")
+
+num = 1 
+for j in range (M):
+    for i in range (N):
+        matrix_1 [i][j] = num
+        num +=1
+printMatrix (matrix_1)
+
+printSeparator ("*","matrix3")
+
+num = 1 
+for j in range (M-1,-1,-1):
+    for i in range(N):
+        matrix_1 [i][j] = num
+        num +=1
+printMatrix (matrix_1)
+
+#COMPLETE
