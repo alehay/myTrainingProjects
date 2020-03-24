cc8d5c380e90e759be4edead32b65ab57758919c
alehay <Alehaymkm@gmail.com>
2020-03-24 13:23:40 +0300

    решение задачи 18 матрицы

--- Py/exercises/matrix/matrix_ex_18_polykov_p10.py
+++ Py/exercises/matrix/matrix_ex_18_polykov_p10.py
@@ -13,6 +13,17 @@
 
 
 заметки по решению :
+    
+суть в заполнение от угла, надо подумать над алгоримтом . 
+
+певый элемент индекс  [0,0]
+вотопрой эелемент [1,0] и [0,1]
+третий элемент [2,0] [0,2] и [1,1]
+
+идея ? а что если использовать сумму индексов как определитель элемента?
+(идея себя оправдала )
+надо подогнать решенеи под другие случаи
+
 """
 
 
@@ -30,3 +41,35 @@ def printMatrix ( A ):
         for j in range(len(A[i])):
             print ("{:3d}".format(A[i][j]), end ="")
         print() #для перевода на новую строку. 
+        
+N=9 #3
+M=16 #4
+
+matrix_1 = [[0]*M for i in range(N)]
+printSeparator ("*","matrix1 ")
+
+for i in range(N):
+    for j in range(M):
+        matrix_1 [i][j] = i+j+1 
+printMatrix (matrix_1)
+
+printSeparator ("*","matrix2")
+
+matrix_2 = [[0]*M for i in range(N)]
+for i in range(N):
+    for j in range(M):
+        matrix_2 [i][j] = i+(M-j)
+
+printMatrix (matrix_2)
+
+printSeparator ("*","matrix3")
+
+
+matrix_3 = [[0]*M for i in range(N)]
+for i in range(N):
+    for j in range(M):
+        matrix_3 [i][j] = (N-i)+(M-j)-1
+
+printMatrix (matrix_3)
+
+#COMPLETE!
